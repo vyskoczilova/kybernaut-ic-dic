0da84cb5e63da3c735bcb4d39806e7603af93803
Karolína Vyskočilová <karolina@kybernaut.cz>
2023-02-02 20:02:52 +0000

    composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,33 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'name' => 'vyskocilova/kybernaut-ic-dic',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7553138b2ad7382a992461f06aebe6485bc34f38',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
     ),
-    'reference' => '3b7dc6fdc1d861aee27b43f53cb3990668ed5579',
-    'name' => 'vyskocilova/kybernaut-ic-dic',
-  ),
-  'versions' => 
-  array (
-    'ibericode/vat' => 
-    array (
-      'pretty_version' => '2.0.5',
-      'version' => '2.0.5.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'b12b6e42c4d57f98189267d744025b9c7539ccdd',
+    'versions' => array(
+        'ibericode/vat' => array(
+            'pretty_version' => '2.0.5',
+            'version' => '2.0.5.0',
+            'reference' => 'b12b6e42c4d57f98189267d744025b9c7539ccdd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ibericode/vat',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vyskocilova/kybernaut-ic-dic' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7553138b2ad7382a992461f06aebe6485bc34f38',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
-    'vyskocilova/kybernaut-ic-dic' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '3b7dc6fdc1d861aee27b43f53cb3990668ed5579',
-    ),
-  ),
 );
