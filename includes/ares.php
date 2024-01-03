04c4d6c282fd3cd262936453e461751396e62137
Lukáš Tomoszek <59371331+lukas-tomoszek@users.noreply.github.com>
2024-01-03 13:36:16 +0100

    Use new ARES API

--- includes/ares.php
+++ includes/ares.php
@@ -2,7 +2,7 @@
 
 /**
  * MFCR INFO ARES API
- * http://wwwinfo.mfcr.cz/ares/ares_xml.html.cz#k3
+ * https://ares.gov.cz/stranky/vyvojar-info
  * 
  * ADDITIONAL CREDITS
  * https://github.com/svecon/web-utilities/blob/master/Ares/Ares.php
@@ -18,49 +18,33 @@ if ( ! function_exists( 'woolab_icdic_ares') ) {
             return array( 'error' => __('Business ID not set.', 'woolab-ic-dic'));
         }
 
-        $url = 'http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=' . $ico;
+        $url = 'https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/' . $ico;
         $response = wp_remote_get( $url );
 
         if ( ! is_wp_error( $response ) ) {
-            
+
+            $status_code = wp_remote_retrieve_response_code( $response );
             $body = wp_remote_retrieve_body($response);
-            $xml  = simplexml_load_string($body);
+            $data = json_decode($body);
 
-            if ( $xml ) {
+            if ( $status_code == 200 && $data ) {
 
-                $ns = $xml->getDocNamespaces(); 
-                $data = $xml->children($ns['are']);
-                $data = $data->children($ns['D'])->VBAS;
+                $return = array( 'error' => false );
+                $return['spolecnost'] = $data->obchodniJmeno;
+                $return['ico'] = $data->ico;
+                $return['dic'] = $data->dic;
 
-                if ( $data ) {
+                $address = $data->sidlo;
+                $return['adresa'] = explode(',', $address->textovaAdresa)[0];
+                $return['psc'] = $address->psc;
+                $return['mesto'] = $address->nazevObce;
 
-                    $return = array( 'error' => false );
-                    $return['spolecnost'] = $data->OF->__toString();
-                    $return['ico'] = $data->ICO->__toString();
-                    $return['dic'] = $data->DIC->__toString();
-
-                    $cp_1 = $data->AA->CD->__toString();
-                    $cp_2 = $data->AA->CO->__toString();
-                    $cp = ( $cp_2 != "" ? $cp_1."/".$cp_2 : $cp_1 );
-                    $cp = (empty($cp)?$data->AA->CA->__toString():$cp);
-
-                    $street = $data->AA->NU->__toString() ? $data->AA->NU->__toString() : $data->AA->NCO->__toString();
-                    $return['adresa'] = $street . ' ' . $cp;
-
-                    $return['psc'] = $data->AA->PSC->__toString();
-                    $return['mesto'] = $data->AA->N->__toString();
-
-                } else {
-                    
-                    $return = array( 'error' => __('Entity doesn\'t exist in ARES.', 'woolab-ic-dic'));
-                    
-                }
-
+            } elseif ( $status_code == 404 ) {
+                $return = array( 'error' => __('Entity doesn\'t exist in ARES.', 'woolab-ic-dic'));
             } else {
                 $return = array( 'error' => __('ARES is not responding', 'woolab-ic-dic'));
-
             }
-            
+
         } else {
             $return = array( 'error' => __('WP ERROR, can\'t connect.', 'woolab-ic-dic'));
         }
